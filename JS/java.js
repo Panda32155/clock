31676a4bdf129fe37d4d1ba477f649a0993c585e
Panda32155 <142974704+Panda32155@users.noreply.github.com>
2024-05-04 20:21:02 +0400

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,19 @@
+document.querySelector('.item1').addEventListener('click', function() {
+    window.open ( "item1.html", "_blank");
+  });
+document.querySelector('.item2').addEventListener('click', function() {
+    window.open ( "item2.html", "_blank"); 
+  });
+document.querySelector('.item3').addEventListener('click', function() {
+    window.open ( "item3.html", "_blank");
+  });
+document.querySelector('.item4').addEventListener('click', function() {
+    window.open ( "item4.html", "_blank");
+  });
+document.querySelector('.item5').addEventListener('click', function() {
+    window.open ( "item5.html", "_blank");
+  });
+document.querySelector('.item6').addEventListener('click', function() {
+    window.open ( "item6.html", "_blank");
+  });
+  
